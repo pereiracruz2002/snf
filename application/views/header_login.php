7ea41beddc7cae863551e2fa803cd3ff530bde2a
Flávio <flavio@wvtodoz.com.br>
2014-11-11 16:15:13 -0200

    tela de login

---
+++ application/views/header_login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Sistema Nota Fiscal</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url() ?>assets/css/sb-admin.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo base_url() ?>assets/css/plugins/morris.css" rel="stylesheet">
+     <link href="<?php echo base_url() ?>assets/fontsfont-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script>
+      var base_url = '<?php echo base_url() ?>';
+    </script>
+
+
+
+<body>
--- application/views/login.php
+++ application/views/login.php
@@ -1,9 +1,38 @@
-<?php include_once(dirname(__FILE__).'/header.php'); ?>
-<form class="form-signin" role="form" method="post">
+<?php include_once(dirname(__FILE__).'/header_login.php'); ?>
+<!--<form class="form-signin" role="form" method="post">
   <?php if(isset($erro)) print box_alert($erro); ?>
   <h2 class="form-signin-heading">Identifique-se</h2>
   <input type="text" class="form-control" name="login" placeholder="Login" required autofocus>
   <input type="password" class="form-control" name="senha" placeholder="Senha" required>
   <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
-</form>
+</form>-->
+<div class="container">
+    <div class="row">
+		<div class="col-md-4 col-md-offset-4">
+    		<div class="panel panel-default">
+			  	<div class="panel-heading">
+			    	<h3 class="panel-title">Please sign in</h3>
+			 	</div>
+			  	<div class="panel-body">
+			    	<form accept-charset="UTF-8" role="form">
+                    <fieldset>
+			    	  	<div class="form-group">
+			    		    <input class="form-control" placeholder="E-mail" name="email" type="text">
+			    		</div>
+			    		<div class="form-group">
+			    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
+			    		</div>
+			    		<div class="checkbox">
+			    	    	<label>
+			    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
+			    	    	</label>
+			    	    </div>
+			    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+			    	</fieldset>
+			      	</form>
+			    </div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php include_once(dirname(__FILE__).'/footer.php'); ?>
